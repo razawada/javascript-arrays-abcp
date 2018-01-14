6f23106dff02754777f89af2ce34c367b22e0915
Rance Zawada <razawada@gmail.com>
2018-01-14 17:19:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,21 +1,21 @@
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element) {
   return array.unshift(element);
-}
+};
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
 
-}
+};
 
 function addElementToEndOfArray(array, element) {
 
-}
+};
 
 function destructivelyAddEementToEndOfArray(array, element) {
 
-}
+};
 
 function accessElementInArray(array, index) {
   return array[index];
-}
+};
