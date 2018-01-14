917e1e02e6cb4c9bd244f282426d00240d2df63b
Rance Zawada <razawada@gmail.com>
2018-01-14 17:12:33 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,21 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array, element) {
+  return array.unshift(element);
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+
+}
+
+function addElementToEndOfArray(array, element) {
+
+}
+
+function destructivelyAddEementToEndOfArray(array, element) {
+
+}
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
