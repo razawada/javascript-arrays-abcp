36ca44c5e8e710bd726dbe9c6d246992a308250a
Rance Zawada <razawada@gmail.com>
2018-01-14 18:24:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -26,4 +26,8 @@ function accessElementInArray(array, index) {
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift();
   return array;
+};
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1);
 }
