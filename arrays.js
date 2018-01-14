d3f8fda6532370823abd1d6c81d0bcc3eb3a2b74
Rance Zawada <razawada@gmail.com>
2018-01-14 18:05:40 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(array, element) {
-  var newArray = [...element, array];
-  return newArray
+  return [element, ...array];
 };
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
